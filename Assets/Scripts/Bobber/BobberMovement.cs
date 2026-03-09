3305dfb754ac2548d462fb233a4e64070e5a2962
rosetrussler <t017235p@student.staffs.ac.uk>
2026-03-09 17:37:46 +0000

    Done work on bobber states

--- Assets/Scripts/Bobber/BobberMovement.cs
+++ Assets/Scripts/Bobber/BobberMovement.cs
@@ -1,26 +1,47 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class BobberMovement : MonoBehaviour
 {
+
     [SerializeField] private float m_bobberMoveSpeed;
+    [SerializeField] private float m_moveCooldown;
+    private float m_moveTimer;
 
     //this is the magnitude of the vector got by mouse position
     private Vector2 m_moveDirection;
 
+
+    public event Action OnCast;
+    public event Action OnStartCast;
+    public event Action OnCancelCast;
+
+    private void Awake()
+    {
+        GetComponent<BobberState>().OnStopMovement += StopMovementHandler;
+    }
+
     private void Update()
     {
-           transform.position += new Vector3(m_moveDirection.x * m_bobberMoveSpeed * Time.deltaTime, m_moveDirection.y * m_bobberMoveSpeed * Time.deltaTime, 0);
+        transform.position += new Vector3(m_moveDirection.x * m_bobberMoveSpeed * Time.deltaTime, m_moveDirection.y * m_bobberMoveSpeed * Time.deltaTime, 0);
+        m_moveTimer = m_moveCooldown;
     }
 
     public void HandleMove(InputAction.CallbackContext ctx) //this is called ALOT per second, in future improve this
     {
-        //should only do when cast
-        if (ctx.performed)
+
+        if (Time.time > m_moveTimer)   //only do when not in cooldown
         {
-            Debug.Log("Move: " + ctx.ReadValue<Vector2>());
-            m_moveDirection = ctx.ReadValue<Vector2>();
+            //should only do when cast
+            if (ctx.performed)
+            {
+                Debug.Log("Move: " + ctx.ReadValue<Vector2>());
+                m_moveDirection = ctx.ReadValue<Vector2>();
+                m_moveTimer = Time.time + m_moveCooldown;
+            }
         }
+
 
     }
 
@@ -30,6 +51,15 @@ public class BobberMovement : MonoBehaviour
         if (ctx.performed)
         {
             Debug.Log("Cast performed");
+            OnCast?.Invoke();
+        }
+        else if (ctx.started)
+        {
+            OnStartCast?.Invoke();
+        }
+        else if (ctx.canceled)
+        {
+            OnCancelCast?.Invoke();
         }
     }
 
@@ -39,6 +69,24 @@ public class BobberMovement : MonoBehaviour
         if (ctx.performed)
         {
             Debug.Log("ClickOnce performed");
+        }
+    }
+
+    /// <summary>
+    /// set the move vector to 0
+    /// </summary>
+    private void StopMovementHandler()
+    {
+        m_moveDirection = Vector3.zero;
+    }
+
+    private void OnDestroy()
+    {
+        //unsub from events
+        BobberState bobberState = GetComponent<BobberState>();
+        if (bobberState != null)
+        {
+            bobberState.OnStopMovement -= StopMovementHandler;
         }
     }
 }
---
+++ Assets/Scripts/Bobber/BobberState.cs
@@ -0,0 +1,104 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+/// <summary>
+/// The state of the bobber, this requires BobberMovement and PlayerInput components
+/// </summary>
+[RequireComponent(typeof(BobberMovement))]
+[RequireComponent (typeof(PlayerInput))]
+public class BobberState : MonoBehaviour
+{
+    //what the bobber is doing
+    enum BobberStateMachine
+    {
+        outOfWater,
+        casting,
+        inWater,
+        reeling
+    }
+
+    private BobberStateMachine m_bobberState;
+    private PlayerInput m_playerInput;
+
+    public event Action OnStopMovement;
+
+    private void Awake()
+    {
+        m_playerInput = GetComponent<PlayerInput>();
+        BobberMovement bobberMovement = GetComponent<BobberMovement>();
+        bobberMovement.OnCast += SwitchToInWater;
+        bobberMovement.OnStartCast += SwitchToCasting;
+        bobberMovement.OnCancelCast += SwitchToOutOfWater;
+        
+    }
+
+    private void Start()
+    {
+        //bobber starts out of water 
+        SwitchToOutOfWater();
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) //if bobber touches fish it is reeling time
+    {
+        if (collision.CompareTag("Fish"))
+        {
+            SwitchToReeling();
+        }
+    }
+
+    /// <summary>
+    /// Switch the state machine and handle what controls are enabled
+    /// </summary>
+    private void SwitchToOutOfWater()
+    {
+        if (m_bobberState != BobberStateMachine.inWater)    //this is to stop the release of the cast button from setting the bobber to out of water, this shouldn't affect anything else as these states should not directly transition between eachother
+        {
+            m_bobberState = BobberStateMachine.outOfWater;
+            m_playerInput.actions.FindAction("Move").Disable();
+            m_playerInput.actions.FindAction("ClickOnce").Disable();
+            m_playerInput.actions.FindAction("Cast").Enable();
+        }
+    }
+
+    /// <summary>
+    /// Switch the state machine to casting 
+    /// </summary>
+    private void SwitchToCasting()
+    {
+        m_bobberState = BobberStateMachine.casting;
+    }
+
+    /// <summary>
+    /// Switch the state machine to inWater and handle what controls are enabled
+    /// </summary>
+    private void SwitchToInWater()
+    {
+        m_bobberState = BobberStateMachine.inWater;
+        m_playerInput.actions.FindAction("Cast").Disable();
+        m_playerInput.actions.FindAction("ClickOnce").Disable();
+        m_playerInput.actions.FindAction("Move").Enable();
+    }
+
+    private void SwitchToReeling()
+    {
+        m_bobberState = BobberStateMachine.reeling;
+        m_playerInput.actions.FindAction("Move").Disable();
+        m_playerInput.actions.FindAction("ClickOnce").Enable();
+        OnStopMovement?.Invoke();
+    }
+
+    private void OnDestroy()
+    {
+        //unsubscribe from events
+        BobberMovement bobberMovement = GetComponent<BobberMovement>();
+        if (bobberMovement != null)
+        {
+            bobberMovement.OnCast -= SwitchToInWater;
+            bobberMovement.OnStartCast -= SwitchToCasting;
+            bobberMovement.OnCancelCast -= SwitchToOutOfWater;
+        }
+    }
+}
